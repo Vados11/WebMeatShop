a417a03506a21c64a5c766ca307b012f4d3d6a19
Vados11 <vlad.davydenko.2002@gmail.com>
2023-02-14 13:58:26 +0200

    add package.json

--- practise.js
+++ practise.js
@@ -1,12 +1,56 @@
-const btn = document.querySelector(".btn1");
-const remove = document.querySelector(".btn2");
+// Scoop function A() {
+//   let a = 1;
 
-const onbuttonClick = function () {
-  console.log("hello a u click me!");
-};
+//   function B() {
+//     let b = 2;
 
-btn.addEventListener("click", onbuttonClick);
+//     function C() {
+//       let c = 3;
 
-remove.addEventListener("click", () => {
-  btn.removeEventListener("click", onbuttonClick);
-});
+//       console.log("func C:", a, b, c);
+//     }
+
+//     C();
+//     console.log("func B:", a, b);
+//   }
+//   B();
+//   console.log("func A:", a);
+// }
+
+// A();
+
+// Замиканія function createCounter() {
+//   let counter = 0;
+
+//   function count() {
+//     counter += 1;
+//     return counter;
+//   }
+//   return count;
+// }
+
+// const increment = createCounter();
+
+// const c1 = increment();
+// const c2 = increment();
+// const c3 = increment();
+
+// console.log("increment", c1, c2, c3);
+
+// const setTimer = (time) => {
+//   const promise = new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       resolve("Done!");
+//     }, time);
+//   });
+
+//   return promise;
+// };
+
+// setTimer(2000)
+//   .then((res) => {
+//     console.log(res, "Hello world");
+//   })
+//   .then(() => {
+//     console.log("Hello bitches");
+//   });
